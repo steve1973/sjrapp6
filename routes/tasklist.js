70d4876ff2372804bd3b1195c14c0b1976b5468d
Steve Reynolds <steve.reynolds@blackpear.com>
2015-08-14 11:37:48 +0100

    Update Task Name

--- routes/tasklist.js
+++ routes/tasklist.js
@@ -8,33 +8,33 @@ function TaskList(connection) {
 }
 
 TaskList.prototype = {
-  showTasks: function(req, res) {
-    task.find({ itemCompleted : false }, function foundTasks(err, items) {
-      res.render('index', { title: 'My ToDo List', tasks: items })
+  showTasks: function (req, res) {
+    task.find({itemCompleted: false}, function foundTasks(err, items) {
+      res.render('index', {title: 'Steve\'s ToDo List', tasks: items})
     });
   },
 
-  addTask: function(req,res) {
+  addTask: function (req, res) {
     var item = req.body;
     var newTask = new task();
     newTask.itemName = item.itemName;
     newTask.itemCategory = item.itemCategory;
     newTask.save(function savedTask(err) {
-      if(err) {
+      if (err) {
         throw err;
       }
     });
     res.redirect('/');
   },
 
-  completeTask: function(req,res) {
+  completeTask: function (req, res) {
     var completedTasks = req.body;
-    for(taskId in completedTasks) {
-      if(completedTasks[taskId]=='true') {
-        var conditions = { _id: taskId };
-        var updates = { itemCompleted: completedTasks[taskId] };
+    for (taskId in completedTasks) {
+      if (completedTasks[taskId] == 'true') {
+        var conditions = {_id: taskId};
+        var updates = {itemCompleted: completedTasks[taskId]};
         task.update(conditions, updates, function updatedTask(err) {
-          if(err) {
+          if (err) {
             throw err;
           }
         });
